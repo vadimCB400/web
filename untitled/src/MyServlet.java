5eccf051e47c93fdcc054832bb98d5dd4fe9ef59
vadim <vadim.ayup0v@yandex.ru>
2019-10-01 20:21:02 +0300

    done

---
+++ untitled/src/MyServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher view = req.getRequestDispatcher("index.jsp");
+        view.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+
+        if(login.equals("vadim") && password.equals("123")){
+            resp.sendRedirect("../okay.jsp");
+        }else{
+            resp.sendRedirect("../index");
+        }
+    }
+}
---
+++ untitled/web/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: vadim
+  Date: 25.09.2019
+  Time: 11:14
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <form method="POST" action="/login">
+  <input type="text" name="login" placeholder="login">
+  <br><input type="password" name="password" placeholder="password"></br>
+  <input type="submit" value="send">
+  </form>
+  </body>
+</html>
