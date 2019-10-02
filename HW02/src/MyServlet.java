e8ee286d76013d2c58b097ca8d5bb0fdb23ac86e
vadim <vadim.ayup0v@yandex.ru>
2019-10-02 10:41:42 +0300

    done

--- HW02/src/MyServlet.java
+++ HW02/src/MyServlet.java
@@ -6,16 +6,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class MyServlet extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher view = req.getRequestDispatcher("index.jsp");
+        view.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String login = req.getParameter("login");
         String password = req.getParameter("password");
 
-        if(login.equals("vadim")== true && password.equals("123")==true){
-            req.getRequestDispatcher("/okay.jsp").forward(req, resp);
+        if(login.equals("vadim") && password.equals("123")){
+            resp.sendRedirect("okay.jsp");
         }else{
-            req.getRequestDispatcher("/index.jsp").forward(req, resp);
+            resp.sendRedirect("login");
         }
     }
 }
